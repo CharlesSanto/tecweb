9c021ee69df62a9c22d5a7d5125798a81ca42db1
Charles Santos <coliveirasantos1234@gmail.com>
2026-04-03 00:30:42 -0300

    .

--- praticas/aluno-online-vanilla/src/main.js
+++ praticas/aluno-online-vanilla/src/main.js
@@ -1,1 +1,21 @@
-import './style.css'
+const inputEmail = document.getElementById('email');
+const inputPassword = document.getElementById('password');
+const formLogin = document.getElementById('form-login');
+const emailError = document.getElementById('email-error');
+const passwordError = document.getElementById('password-error');
+
+
+formLogin.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    emailError.textContent = '';
+    passwordError.textContent = '';
+    
+    if (inputEmail.value === '') {
+        emailError.textContent = 'O campo de email é obrigatório.';
+    }
+
+    if (inputPassword.value === '') {
+        passwordError.textContent = 'O campo de senha é obrigatório.';
+    }
+})
